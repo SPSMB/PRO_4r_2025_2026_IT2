44cf6835db19edb9cc63dd11ac20bce92b8c4334
zimmerhakl <tomas.zimmerhakl@gmail.com>
2025-11-14 11:33:51 +0100

    14.11. Zapis do souboru - uprava filtrovani vlakovych stanic.

--- 25_Stanice.cpp
+++ 25_Stanice.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-void nactiStanice(string jmenoSouboru){
+void zapisDoSouboru(string jmenoSouboru, stringstream & s1){
+	ofstream f2(jmenoSouboru);
+	if(f2.is_open()){
+		f2 << s1.str(); // cely obsah streamu poslu do souboru
+	} else {
+		cout << "Soubor " << jmenoSouboru 
+			 << " se nepovedlo otevrit." << endl; 
+	}
+
+	f2.close();
+}
+
+void nactiStanice(string jmenoSouboru, stringstream & s1){
 	ifstream f1(jmenoSouboru);
 	string radek;
 	int counter = 0;
+	int praha = 0;
 	if(!f1.is_open()){
 		cout << "Nepovedlo se otevrit soubor " << jmenoSouboru 
 			 << ". Program se ukonci." << endl;
@@ -21,18 +35,23 @@ void nactiStanice(string jmenoSouboru){
 		if(radek.find("Praha") != string::npos){
 			string id = radek.substr(0,5);
 			string jmeno = radek.substr(6);
-			cout << jmeno << " (" << id << ")" << endl;
+			s1 << jmeno << " (" << id << ")" << endl;
+			praha++;
 		}	
 	}
 	cout << "Pocet vsech stanic je " << counter << endl;
+	cout << "Pocet prazskych stanic je " << praha << endl;
 	f1.close();
 }
 
 
 int main(void){
 
-	string jmenoSouboru = "25_Stanice_slovnik_2025.csv";
-	nactiStanice(jmenoSouboru);
+	stringstream s1;
+	string souborVstup = "25_Stanice_slovnik_2025.csv";
+	string souborVystup = "25_Vystup.txt";
+	nactiStanice(souborVstup, s1);
+	zapisDoSouboru(souborVystup, s1);
 
 	return 0;
 }
